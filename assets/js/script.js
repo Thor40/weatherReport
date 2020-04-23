fdcb7a2e16d8c4c1481e1eab8109f7a5c92b98e2
Thor40 <nicholas.kosik@gmail.com>
2020-04-23 17:39:56 -0500

    hotfix

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,10 @@ var userFormEl = document.querySelector("#user-form")
 var cityInputEl = document.querySelector("#city-input")
 var weatherContainerEl = document.querySelector("#weather-container");
 var weatherSearchTerm = document.querySelector("#weather-search-term");
+var currentDate = moment().format("dddd, MMMM Do YYYY");
 
 var getCity = function(city) {
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=e30b91e5b089d12b97f22fef450b5850";
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=e30b91e5b089d12b97f22fef450b5850";
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
@@ -20,23 +21,27 @@ var getCity = function(city) {
     });
 };
 
-var displayCity = function(main, searchTerm) {
-    console.log(main);
+var displayCity = function(data, searchTerm) {
+    console.log(data);
     console.log(searchTerm);
 
+    var setCurrentDate = currentDate;
+    setCurrentDate.classList = "text-muted"
+    
     weatherContainerEl.textContent = "";
-    weatherSearchTerm.textContent = searchTerm;
+    weatherSearchTerm.textContent = searchTerm + setCurrentDate;
 
     // div holding weather repot
     var weatherEl = document.createElement("div");
     weatherEl.classList = "flex-row list-group align-left";
     // title of city
     var titleEl = document.createElement("span");
+    titleEl.classList = "flex-row";
     titleEl.textContent = name;
     // weather info
     var tempEl = document.createElement("li");
     tempEl.classList = "list-group-item"
-    tempEl.textContent = main.temp;
+    tempEl.textContent = "Temperature: " + data.main.temp + "°";
 
     weatherEl.appendChild(titleEl);
     weatherEl.appendChild(tempEl);
